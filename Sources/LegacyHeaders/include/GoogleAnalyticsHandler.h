c29ef87c9f48591d2fd01e3c2bd0e31586c117ef
Ron Tabachnik <rontabachnik@gmail.com>
2025-04-29 18:54:39 +0300

    Comment out google analytics handler

--- Sources/LegacyHeaders/include/GoogleAnalyticsHandler.h
+++ Sources/LegacyHeaders/include/GoogleAnalyticsHandler.h
@@ -1,35 +1,35 @@
+////
+////  GoogleAnalyticsHandler.h
+////  BuyMe
+////
+////  Created by poccaDot on 6/15/14.
+////  Copyright (c) 2014 poccaDot. All rights reserved.
+////
 //
-//  GoogleAnalyticsHandler.h
-//  BuyMe
+//#import <Foundation/Foundation.h>
+////#import "GAI.h"
+////#import "GAIDictionaryBuilder.h"
+////#import "GAIFields.h"
+//#import <FirebaseAnalytics/FirebaseAnalytics.h>
 //
-//  Created by poccaDot on 6/15/14.
-//  Copyright (c) 2014 poccaDot. All rights reserved.
+//@interface GoogleAnalyticsHandler : NSObject
+//{
+//    //id<GAITracker> tracker;
+//}
 //
-
-#import <Foundation/Foundation.h>
-//#import "GAI.h"
-//#import "GAIDictionaryBuilder.h"
-//#import "GAIFields.h"
-#import <FirebaseAnalytics/FirebaseAnalytics.h>
-
-@interface GoogleAnalyticsHandler : NSObject
-{
-    //id<GAITracker> tracker;
-}
-
-//@property (nonatomic, retain) id<GAITracker> tracker;
-
-+ (GoogleAnalyticsHandler *)sharedInstance;
-
-- (void)setUserProperty:(NSString*)property value:(NSString*)value;
-- (void)sendGAEventWithCategory:(NSString *)category Action:(NSString *)action Label:(NSString *)label andValue:(int)value withProperties:(NSDictionary *)properties;
-- (void)sendGAEventWithCategory:(NSString *)category Action:(NSString *)action Label:(NSString *)label andValue:(int)value;
-- (void)trackPageView:(NSString*)pageName;
-- (void)trackPageView:(NSString*)pageName pageUrl:(NSString*)pageUrl;
-- (void)trackPageView:(NSString*)pageName pageClass:(NSString*)pageClass;
-
-- (void)sendGAEventWithEventName:(NSString *)eventName withProperties:(NSDictionary *)properties;
-
-- (void)purchaseCompleted:(NSString*)transactionId name:(NSString*)name sku:(NSString*)sku category:(NSString*)category price:(NSNumber*)price quantity:(NSInteger)quantity currencyCode:(NSString*)currencyCode;
-
-@end
+////@property (nonatomic, retain) id<GAITracker> tracker;
+//
+//+ (GoogleAnalyticsHandler *)sharedInstance;
+//
+//- (void)setUserProperty:(NSString*)property value:(NSString*)value;
+//- (void)sendGAEventWithCategory:(NSString *)category Action:(NSString *)action Label:(NSString *)label andValue:(int)value withProperties:(NSDictionary *)properties;
+//- (void)sendGAEventWithCategory:(NSString *)category Action:(NSString *)action Label:(NSString *)label andValue:(int)value;
+//- (void)trackPageView:(NSString*)pageName;
+//- (void)trackPageView:(NSString*)pageName pageUrl:(NSString*)pageUrl;
+//- (void)trackPageView:(NSString*)pageName pageClass:(NSString*)pageClass;
+//
+//- (void)sendGAEventWithEventName:(NSString *)eventName withProperties:(NSDictionary *)properties;
+//
+//- (void)purchaseCompleted:(NSString*)transactionId name:(NSString*)name sku:(NSString*)sku category:(NSString*)category price:(NSNumber*)price quantity:(NSInteger)quantity currencyCode:(NSString*)currencyCode;
+//
+//@end
